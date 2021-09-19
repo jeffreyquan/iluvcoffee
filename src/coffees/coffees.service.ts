0630956e3672b52e282f23a3ab8b7f03b5cbe46e
Jeffrey <jeffrey.quan@hotmail.com>
2021-09-19 22:50:40 +1000

    🐛 fixed import issues in testing

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,5 +1,5 @@
-import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
-import { Inject, Injectable, NotFoundException, Scope } from '@nestjs/common';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Connection, Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
@@ -7,8 +7,6 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
-import { ConfigService, ConfigType } from '@nestjs/config';
-import coffeesConfig from './config/coffees.config';
 
 @Injectable()
 export class CoffeesService {
